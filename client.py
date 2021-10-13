44b5e0a398ee1308f7458dcbb0b022adf5bb914a
MED1616 <dave.gis@hotmail.com>
2021-10-13 12:57:31 +0200

    pickle

--- client.py
+++ client.py
@@ -3,5 +3,7 @@ import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((socket.gethostname(), 4444))#(IP, Port), need to change IP after testing when not on same PC
 
+s.send(bytes("Hello", "utf-8"))
 msg = s.recv(1024)#max size of message
+
 print(msg.decode("utf-8"))
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket
+import pickle
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 4444))#(IP, Port)
@@ -8,5 +9,9 @@ print(socket.gethostbyname(socket.gethostname()))
 while True:
     clientsocket, address = s.accept()
     print(f"Connection from {address} has been established")
+
+    data = clientsocket.recv(1024)
+    data = data.decode("utf-8")
+    print(data)
     clientsocket.send(bytes("Welcome to the server!", "utf-8"))#send data to clientsocket
     clientsocket.close()
